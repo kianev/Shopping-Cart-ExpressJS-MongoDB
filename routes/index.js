640b0a7d5e934d542d4a3b051e1ae384cd7177ed
Kaloian Ianev <kianev@gmail.com>
2017-09-20 17:52:02 +0300

    Fetched data from DB and displayed in browser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+let Product = require('../models/product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function (err, docs) {
+    let productChunks = []
+    let chunkSize = 3
+    for (i = 0; i < docs.length; i += chunkSize){
+      productChunks.push(docs.slice(i, i + chunkSize))
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  })
 });
 
 module.exports = router;
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -21,7 +21,25 @@ let products = [
     title: 'F1 2017',
     description: 'F1 2017 is a racing video game developed and published by Codemasters. It was released for PlayStation 4, Xbox One.',
     price: 20
-  })
+  }),
+  new Product({
+    imagePath: 'https://gpstatic.com/acache/32/85/2/uk/packshot-248d06dee3d0a2b332bd6a47e3943f59.jpg',
+    title: 'Far Cry 5',
+    description: 'Far Cry 5 is a first-person shooter action-adventure video game developed by Ubisoft Montreal.',
+    price: 5
+  }),
+  new Product({
+    imagePath: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWcNur?ver=97b9',
+    title: 'F1 2017',
+    description: 'F1 2017 is a racing video game developed and published by Codemasters. It was released for PlayStation 4, Xbox One.',
+    price: 2
+  }),
+  new Product({
+    imagePath: 'https://vignette.wikia.nocookie.net/callofduty/images/8/8a/CoD4_boxart.png/revision/latest?cb=20110107234635',
+    title: 'Call of Duty 4',
+    description: 'Best single person shooter!',
+    price: 100
+  }),
   ]
 
 let done = 0;
