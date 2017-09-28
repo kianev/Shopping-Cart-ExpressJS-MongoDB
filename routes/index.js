10f8ab16f9f1566ca4b0a0742051d275042b15a5
Kaloian Ianev <kianev@gmail.com>
2017-09-28 12:43:56 +0200

    added signin with validation

--- routes/index.js
+++ routes/index.js
@@ -29,10 +29,20 @@ router.post('/user/signup',
     successRedirect: '/user/profile',
     failureRedirect: '/user/signup',
     failureFlash: true })
-);
+)
 
 router.get('/user/profile', function (req, res, next) {
   res.render('user/profile')
 })
 
+router.get('/user/signin', function (req, res, next) {
+  let messages = req.flash('error')
+  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
+})
+
+router.post('/user/signin', passport.authenticate('local.signin', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signin',
+  failureFlash: true
+}))
 module.exports = router;
